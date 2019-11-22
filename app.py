2dae1734c7546712d17f18d1b290337f523818ba
Flionic <flionicj@gmail.com>
2019-11-23 01:15:58 +0200

    Piastix html type payment

--- app.py
+++ app.py
@@ -12,9 +12,33 @@ def index():
     return render_template("payment.html")
 
 
-def pay_sign(pay_request):
+@app.route('/action/payment', methods=['POST'])
+def act_payment():
+    # pm = payment
+    pm_request = request.form.to_dict(flat=True)
+    pm_request['shop_id'] = 5
+    pm_request['shop_order_id'] = 101
+
+    print(request.form)
+    print(request.form.to_dict(flat=True))
+
+    if request.form.get('currency') is None:
+        return 404
+    elif request.form.get('currency') == 'EUR':
+        pm_request['currency'] = 978
+        return piastix_form(pm_request)
+    elif request.form.get('currency') == 'USD':
+        pm_request['currency'] = 840
+        return piastix_api_bill(pm_request)
+    elif request.form.get('currency') == 'RUB':
+        pm_request['currency'] = 643
+        return piastix_api_invoice(pm_request)
+    else:
+        return jsonify({"response": 0})
+
+
+def pm_sign(pay_request, keys_required):
     secret = 'SecretKey01'
-    keys_required = ["shop_id", "payway", "amount", "currency", "shop_order_id"]
 
     sign = hashlib.sha256()
     keys_required.sort()
@@ -23,6 +47,25 @@ def pay_sign(pay_request):
     sign.update(secret.encode())
 
     return sign.hexdigest()
+
+
+def piastix_form(payment):
+    keys_required = ["amount", "currency", "shop_id", "shop_order_id"]
+    payment['sign'] = pm_sign(payment, keys_required)
+
+    return jsonify({'type': 'html', 'data': render_template('ajax/payform.html', payment=payment)})
+
+
+def piastix_api_bill(payment):
+    keys_required = ["amount", "currency", "shop_id", "shop_order_id"]
+
+    return jsonify({"response": "piastix_api_bill"})
+
+
+def piastix_api_invoice(payment):
+    keys_required = ["shop_amount", "shop_currency", "shop_id", "shop_order_id", "payer_currency"]
+
+    return jsonify({"response": "piastix_api_invoice"})
 
 
 if __name__ == '__main__':
