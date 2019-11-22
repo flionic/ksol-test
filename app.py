8aac4095bf97551fcb6b06bfb273c9ba0f3a8106
Flionic <flionicj@gmail.com>
2019-11-23 00:31:47 +0200

    Generate pay sign

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 # app.py
-from flask import Flask, render_template
+import hashlib
+
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,6 +10,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("payment.html")
+
+
+def pay_sign(pay_request):
+    secret = 'SecretKey01'
+    keys_required = ["shop_id", "payway", "amount", "currency", "shop_order_id"]
+
+    sign = hashlib.sha256()
+    keys_required.sort()
+    sign_values = [str(pay_request[i]) for i in keys_required]
+    sign.update(':'.join(sign_values).encode())
+    sign.update(secret.encode())
+
+    return sign.hexdigest()
 
 
 if __name__ == '__main__':
